46045c186e9612ec41cfbac2c57aa76f8ef776fd
dmrasf <dmrasf@qq.com>
2020-03-01 21:43:15 +0800

    com

--- fish/config.fish
+++
@@ -1,4 +0,0 @@
-set PATH /home/dmr/Proc/pycharm-community-2019.3.1/bin $PATH
-set PATH /home/dmr/Proc/scitools/bin/linux64 $PATH
-set PATH /home/dmr/.local/bin $PATH
-set PATH /home/dmr/.gem/ruby/2.7.0/bin $PATH
